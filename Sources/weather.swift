23440b500e884d95327fbd6b9c6b51744cfcb5e8
Chris Bailey <chris@bailey.mx>
2024-06-17 09:25:40 -0400

    strip down

--- Sources/weather.swift
+++ Sources/weather.swift
@@ -7,10 +7,15 @@ import SwiftLocation
 @raycast
 func location() async throws -> Double? {
   let location = Location()
-  return await location.locationServicesEnabled ? 1.0 : 0.0
   let current: Tasks.ContinuousUpdateLocation.StreamEvent = try await location.requestLocation(accuracy: [
     .horizontal(10000) // has an horizontal accuracy of 100 meters or lower
   ], timeout: 10)
   return current.location?.coordinate.latitude ??
     current.locations?.first?.coordinate.latitude ?? 1.0
+}
+
+@raycast
+func enabled() async throws -> Bool {
+  let location = Location()
+  return await location.locationServicesEnabled
 }
